acd5e75ebf619fb3c17a76a12feb11a1d0dcfdda
Anton Dmitriev <antondmit@gmail.com>
2022-10-11 01:49:28 +0300

    Задача 8
    Напишите программу, которая на вход принимает число (N),
    а на выходе показывает все чётные числа от 1 до N.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// The program calculates the parity of a numbers
+
+using System;
+class Program
+{
+    // function without any return type declaration  
+    public int GetNumber(string VarName)
+    {
+        int res;
+        string input;
+        do
+        {
+            Console.WriteLine(String.Format("Enter number: {0} = ", VarName));
+            input = Console.ReadLine();
+        } while (int.TryParse(input, out res) == false);
+        return res;
+    }
+
+    public static void Main(string[] args)
+    {
+        Program pr = new Program(); // Creating a class Object  
+        Console.WriteLine("The program calculates the parity of a numbers.");
+
+        string VarName_N = "N";
+        int N = pr.GetNumber(VarName_N);
+        
+        for (int i = 1; i < N; i++)
+        {
+            if (i%2 == 0) 
+                Console.WriteLine(String.Format("The value {0} is parity.", i));
+        }
+        
+    }
+}
